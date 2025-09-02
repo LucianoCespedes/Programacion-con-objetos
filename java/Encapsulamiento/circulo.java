2b03a5b47b75fb660f62c445d92ce35903bbed6b
LucianoCespedes <145279061+LucianoCespedes@users.noreply.github.com>
2025-09-01 21:49:51 -0300

    Update circulo.java

--- java/Encapsulamiento/circulo.java
+++ java/Encapsulamiento/circulo.java
@@ -1,58 +1,51 @@
-public class circulo extends punto{
+public class circulo {
     
     
-
+    private punto punto;
     private double R;
-    private double D;
-    private double P;
-    private double A;
 
-    public circulo(double x, double y){
-        super(x,y);
-        this.R=0;
-        this.D=0;
-        this.P=0;
-        this.A=0;
+    public circulo(double D, double A){
+        this.punto = new punto(D, A);
+        this.R = Math.sqrt(Math.pow(D, 2)+Math.pow(A,2));
     }
 
     // 
 
-    public void MoverCirculo(double u, double j){
-        super.setX(u);
-        super.setY(j);
+    public void MoverCirculo(double A, double v){
+        punto.Moverpunto(A, v);
     }
 
     public void ubicacion(){
-        System.out.println("X: "+super.getX()+"\n"+"Y: "+super.getY());
+        System.out.println("X: "+punto.getX()+"\n"+"Y: "+punto.getY());
     }
 
     public double Radio()
     {
-        return R = Math.sqrt(Math.pow(super.getX(), 2)+Math.pow(super.getY(),2));
+        return R= Math.sqrt(Math.pow(punto.getX(), 2)+Math.pow(punto.getY(),2));
     }
 
     public double Diametro(){
-        return D= 2*R;
+        return 2*Radio();
     }
     
     public double Perimetro(){
-        return P=2*Math.PI*R;
+        return 2*Math.PI*Radio();
     }
     public double Area(){
-        return A=Math.PI*Math.pow(R, 2);
+        return Math.PI*Math.pow(Radio(), 2);
     }
 
     public static void main(String[] args) {
-        circulo redondo= new circulo(2, 4);
+        circulo redondo= new circulo(2,1);
 
-        redondo.MoverCirculo(2, 22);
-        
-        System.out.println(redondo.Radio());
-        System.out.println(redondo.Diametro());
-        System.out.println(redondo.Perimetro());
-        System.out.println(redondo.Area());
+        System.out.println("Radio: " + redondo.Radio());
+        System.out.println("Diámetro: " + redondo.Diametro());
+        System.out.println("Perímetro: " + redondo.Perimetro());
+        System.out.println("Área: " + redondo.Area());
 
-
+        // mover el círculo
+        redondo.MoverCirculo(5, 5);
+        redondo.ubicacion();
 
     }
 }
